22a40e2a0dd26e20a8d4345a26e7a275e5b21936
daukadolt <daulet.amirkhanov.official@gmail.com>
2020-03-24 16:29:00 +0600

    Реструктуризация папок, соединение mongoose к mongo
    1. Код теперь будет находиться в ./src/.
    2. Разделение логики. ./src/app.js теперь работает с лишь express, первичный
         setup вынесен в ./server.js
    
    Signed-off-by: daukadolt <daulet.amirkhanov.official@gmail.com>

---
+++ server.js
@@ -0,0 +1,15 @@
+require('dotenv/config');
+
+const mongoose = require('mongoose');
+const app = require('./src/app');
+
+mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        const expressPort = process.env.EXPRESS_PORT;
+        app.listen(expressPort, () => { console.log(`Yeti is up and running on port ${expressPort}`); });
+    })
+    .catch((err) => {
+        console.log('Error: mongoose could not connect');
+        console.log(err);
+        process.exit(-1);
+    });
--- src/app.js
+++ src/app.js
@@ -1,8 +1,7 @@
 const express = require('express');
 
 const app = express();
-const port = 8888;
 
 app.get('/', (req, res) => { res.send('App is working!'); });
 
-app.listen(port, () => { console.log(`Yeti is up and running on port ${port}`); });
+module.exports = app;
